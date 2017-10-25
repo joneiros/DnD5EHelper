ff354ed6a56dd2d6b3a3555aef35945c6bee14aa
Jon <jonagander@gmail.com>
2017-10-25 16:08:32 -0300

    Switch MainActivity to LoginActivity. Added new MainActivity and am working with a Drawer Menu. Progress!

---
+++ app/src/main/java/com/example/jongander/dnd5ehelper/LoginActivity.java
@@ -0,0 +1,96 @@
+package com.example.jongander.dnd5ehelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+        //TODO: Remake the D20 image so that it has the word enter on it, rather than having a text view over an image. Then reduce onClickListener s accordingly
+        final TextView tv = (TextView)findViewById(R.id.tvEnter);
+        tv.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String email = ((EditText)findViewById(R.id.etEmail)).getText().toString();
+                String password = ((EditText)findViewById(R.id.etPassword)).getText().toString();
+                if(Authenticate(email, password)){
+                    //TODO Navigate to MainActivity
+                    tv.setText("Logged In!");
+                }else{
+                    tv.setText("Login Failure!");
+                }
+            }
+        });
+        final View view = findViewById(R.id.ivD20);
+        view.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String email = ((EditText)findViewById(R.id.etEmail)).getText().toString();
+                String password = ((EditText)findViewById(R.id.etPassword)).getText().toString();
+                if(Authenticate(email, password)){
+                    //TODO Navigate to MainActivity
+                    tv.setText("Logged In!");
+                }else{
+                    tv.setText("Login Failure!");
+                }
+            }
+        });
+
+
+
+        /*
+        //final TextView mTextView = (TextView) findViewById(R.id.labelMain);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.dnd5eapi.co/api/spells/1/";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try
+                        {
+                            JSONObject jObject = new JSONObject(response);
+                            mTextView.setText(jObject.getString("_id"));
+
+                        }
+                        catch(JSONException ex)
+                        {
+                            //TODO
+                            //ddddddddff
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!" + error);
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+        */
+    }
+
+    private boolean Authenticate(String email, String password){
+        //TODO replace with real authentication
+        if(email.equals("123@123.com") && password.equals("12345")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
--- app/src/main/java/com/example/jongander/dnd5ehelper/MainActivity.java
+++ app/src/main/java/com/example/jongander/dnd5ehelper/MainActivity.java
@@ -1,73 +1,102 @@
 package com.example.jongander.dnd5ehelper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         /*
-        //final TextView mTextView = (TextView) findViewById(R.id.labelMain);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://www.dnd5eapi.co/api/spells/1/";
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-
-                        try
-                        {
-                            JSONObject jObject = new JSONObject(response);
-                            mTextView.setText(jObject.getString("_id"));
-
-                        }
-                        catch(JSONException ex)
-                        {
-                            //TODO
-                            //ddddddddff
-                        }
-
-
-                    }
-                }, new Response.ErrorListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onErrorResponse(VolleyError error) {
-                mTextView.setText("That didn't work!" + error);
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
         */
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close); //toolbar between drawer and "open Navigation drawer"
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    private boolean Authenticate(String email, String password){
-        //TODO replace with real authentication
-        if(email.equals("123@123.com") && password.equals("12345")){
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
             return true;
         }
-        else{
-            return false;
-        }
+
+        return super.onOptionsItemSelected(item);
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
